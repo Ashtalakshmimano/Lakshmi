aaa1ef4846fa0904e034be94be0e38cb9c74f209
Ashtalakshmimano <43747470+Ashtalakshmimano@users.noreply.github.com>
2019-01-25 14:16:40 +0530

    Create largest number.py

---
+++ largest number.py
@@ -0,0 +1,10 @@
+num1=5
+num2=10
+num3=15
+if(num1>=num2):
+  largest=num1
+elif(num2>=num1):
+  largest=num2
+else:
+  largest=num3
+print("the largest number between num1,num2 and num3 is largest")
